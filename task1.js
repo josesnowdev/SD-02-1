14cc241d5619ea51f8e0b7a713b32f9d2794fe02
josesnowdev <j.evelionieves@gmail.com>
2025-12-15 14:33:17 -0600

    Se cargan los archivos modificados del laboratorio

--- task1.js
+++ task1.js
@@ -1,3 +1,7 @@
 // Refer to Task 1 in your Instructions to complete this task
 const prompt = require("prompt-sync")();
-const name = prompt("Ingresa tu nombre");
+const name = prompt("Jose Evelio Nieves Hernandez");
+
+for(let i = 1; i <= 105; i++){
+    console.log(i);
+}
